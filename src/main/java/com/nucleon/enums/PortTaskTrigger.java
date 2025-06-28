0f8224c5c42314f74c543e7efe013f5892460c32
coopermor <coopermorris96@yahoo.com>
2025-06-27 20:05:05 -0700

    Add port tasks from cache

--- src/main/java/com/nucleon/enums/PortTaskTrigger.java
+++ src/main/java/com/nucleon/enums/PortTaskTrigger.java
@@ -2,28 +2,29 @@ package com.nucleon.enums;
 
 import java.util.HashMap;
 import java.util.Map;
+// todo: replace with net.runelite.api.gameval.VarbitID when released
+import com.nucleon.gameval.VarbitID;
 
 public enum PortTaskTrigger
 {
-	TASK_SLOT_0_ID(18017, "port_task_slot_0_id", TaskType.ID, 0),
-	TASK_SLOT_0_TAKEN(18018, "port_task_slot_0_cargo_taken", TaskType.TAKEN, 0),
-	TASK_SLOT_0_DELIVERED(18019, "port_task_slot_0_cargo_delivered", TaskType.DELIVERED, 0),
-	TASK_SLOT_1_ID(18020, "port_task_slot_1_id", TaskType.ID, 1),
-	TASK_SLOT_1_TAKEN(18021, "port_task_slot_1_cargo_taken", TaskType.TAKEN, 1),
-	TASK_SLOT_1_DELIVERED(18022, "port_task_slot_1_cargo_delivered", TaskType.DELIVERED, 1),
-	TASK_SLOT_2_ID(18023, "port_task_slot_2_id", TaskType.ID, 2),
-	TASK_SLOT_2_TAKEN(18024, "port_task_slot_2_cargo_taken", TaskType.TAKEN, 2),
-	TASK_SLOT_2_DELIVERED(18025, "port_task_slot_2_cargo_delivered", TaskType.DELIVERED, 2),
-	TASK_SLOT_3_ID(18026, "port_task_slot_3_id", TaskType.ID, 3),
-	TASK_SLOT_3_TAKEN(18027, "port_task_slot_3_cargo_taken", TaskType.TAKEN, 3),
-	TASK_SLOT_3_DELIVERED(18028, "port_task_slot_3_cargo_delivered", TaskType.DELIVERED, 3),
-	TASK_SLOT_4_ID(18029, "port_task_slot_4_id", TaskType.ID, 4),
-	TASK_SLOT_4_TAKEN(18030, "port_task_slot_4_cargo_taken", TaskType.TAKEN, 4),
-	TASK_SLOT_4_DELIVERED(18031, "port_task_slot_4_cargo_delivered", TaskType.DELIVERED, 4),
-	LAST_CARGO_TAKEN(18033, "port_task_last_cargo_taken", TaskType.OTHER, -1);
+	TASK_SLOT_0_ID(VarbitID.PORT_TASK_SLOT_0_ID, TaskType.ID, 0),
+	TASK_SLOT_0_TAKEN(VarbitID.PORT_TASK_SLOT_0_CARGO_TAKEN,  TaskType.TAKEN, 0),
+	TASK_SLOT_0_DELIVERED(VarbitID.PORT_TASK_SLOT_0_CARGO_DELIVERED,TaskType.DELIVERED, 0),
+	TASK_SLOT_1_ID(VarbitID.PORT_TASK_SLOT_1_ID, TaskType.ID, 1),
+	TASK_SLOT_1_TAKEN(VarbitID.PORT_TASK_SLOT_1_CARGO_TAKEN,  TaskType.TAKEN, 1),
+	TASK_SLOT_1_DELIVERED(VarbitID.PORT_TASK_SLOT_1_CARGO_DELIVERED, TaskType.DELIVERED, 1),
+	TASK_SLOT_2_ID(VarbitID.PORT_TASK_SLOT_2_ID,  TaskType.ID, 2),
+	TASK_SLOT_2_TAKEN(VarbitID.PORT_TASK_SLOT_2_CARGO_TAKEN, TaskType.TAKEN, 2),
+	TASK_SLOT_2_DELIVERED(VarbitID.PORT_TASK_SLOT_2_CARGO_DELIVERED, TaskType.DELIVERED, 2),
+	TASK_SLOT_3_ID(VarbitID.PORT_TASK_SLOT_3_ID, TaskType.ID, 3),
+	TASK_SLOT_3_TAKEN(VarbitID.PORT_TASK_SLOT_3_CARGO_TAKEN,  TaskType.TAKEN, 3),
+	TASK_SLOT_3_DELIVERED(VarbitID.PORT_TASK_SLOT_3_CARGO_DELIVERED,  TaskType.DELIVERED, 3),
+	TASK_SLOT_4_ID(VarbitID.PORT_TASK_SLOT_4_ID, TaskType.ID, 4),
+	TASK_SLOT_4_TAKEN(VarbitID.PORT_TASK_SLOT_4_CARGO_TAKEN, TaskType.TAKEN, 4),
+	TASK_SLOT_4_DELIVERED(VarbitID.PORT_TASK_SLOT_4_CARGO_DELIVERED, TaskType.DELIVERED, 4),
+	LAST_CARGO_TAKEN(VarbitID.PORT_TASK_LAST_CARGO_TAKEN, TaskType.OTHER, -1);
 
 	private final int id;
-	private final String name;
 	private final TaskType type;
 	private final int slot;
 
@@ -32,10 +33,9 @@ public enum PortTaskTrigger
 		ID, TAKEN, DELIVERED, OTHER
 	}
 
-	PortTaskTrigger(int id, String name, TaskType type, int slot)
+	PortTaskTrigger(int id, TaskType type, int slot)
 	{
 		this.id = id;
-		this.name = name;
 		this.type = type;
 		this.slot = slot;
 	}
@@ -43,11 +43,6 @@ public enum PortTaskTrigger
 	public int getId()
 	{
 		return id;
-	}
-
-	public String getName()
-	{
-		return name;
 	}
 
 	public TaskType getType()
---
+++ src/main/java/com/nucleon/gameval/ItemID.java
@@ -0,0 +1,681 @@
+package com.nucleon.gameval;
+
+
+public final class ItemID
+{
+
+	/**
+	 * Crate of steel swords
+	 */
+	public static final int CARGO_CRATE_OF_STEEL_SWORDS = 31680;
+
+	/**
+	 * Crate of raw fish
+	 */
+	public static final int CARGO_CRATE_OF_RAW_FISH = 31681;
+
+	/**
+	 * Crate of bananas
+	 */
+	public static final int CARGO_CRATE_OF_BANANAS = 31682;
+
+	/**
+	 * Crate of rotten bananas
+	 */
+	public static final int CARGO_CRATE_OF_ROTTEN_BANANAS = 31683;
+
+	/**
+	 * Crate of beer
+	 */
+	public static final int CARGO_CRATE_OF_BEER = 31684;
+
+	/**
+	 * Crate of logs
+	 */
+	public static final int CARGO_CRATE_OF_LOGS = 31685;
+
+	/**
+	 * Crate of grog
+	 */
+	public static final int CARGO_CRATE_OF_GROG = 31686;
+
+	/**
+	 * Crate of rum
+	 */
+	public static final int CARGO_CRATE_OF_RUM = 31687;
+
+	/**
+	 * Crate of smuggled rum
+	 */
+	public static final int CARGO_CRATE_OF_SMUGGLED_RUM = 31688;
+
+	/**
+	 * Crate of kegs
+	 */
+	public static final int CARGO_CRATE_OF_KEGS = 31689;
+
+	/**
+	 * Crate of bait
+	 */
+	public static final int CARGO_CRATE_OF_BAIT = 31690;
+
+	/**
+	 * Crate of potatoes
+	 */
+	public static final int CARGO_CRATE_OF_POTATOES = 31691;
+
+	/**
+	 * Crate of compost
+	 */
+	public static final int CARGO_CRATE_OF_COMPOST = 31692;
+
+	/**
+	 * Crate of red salamanders
+	 */
+	public static final int CARGO_CRATE_OF_RED_SALAMANDERS = 31693;
+
+	/**
+	 * Crate of swamp paste
+	 */
+	public static final int CARGO_CRATE_OF_SWAMP_PASTE = 31694;
+
+	/**
+	 * Crate of swords
+	 */
+	public static final int CARGO_CRATE_OF_IDENTIFIED_SWORDS = 31695;
+
+	/**
+	 * Crate of live lobsters
+	 */
+	public static final int CARGO_CRATE_OF_LIVE_LOBSTERS = 31696;
+
+	/**
+	 * Crate of raw lobsters
+	 */
+	public static final int CARGO_CRATE_OF_RAW_LOBSTERS = 31697;
+
+	/**
+	 * Crate of wax
+	 */
+	public static final int CARGO_CRATE_OF_WAX = 31698;
+
+	/**
+	 * Crate of coconuts
+	 */
+	public static final int CARGO_CRATE_OF_COCONUTS = 31699;
+
+	/**
+	 * Crate of vials
+	 */
+	public static final int CARGO_CRATE_OF_VIALS = 31700;
+
+	/**
+	 * Crate of silk
+	 */
+	public static final int CARGO_CRATE_OF_SILK = 31701;
+
+	/**
+	 * Crate of pineapples
+	 */
+	public static final int CARGO_CRATE_OF_PINEAPPLES = 31702;
+
+	/**
+	 * Crate of fresh fish
+	 */
+	public static final int CARGO_CRATE_OF_FRESH_FISH = 31703;
+
+	/**
+	 * Crate of cabbages
+	 */
+	public static final int CARGO_CRATE_OF_CABBAGES = 31704;
+
+	/**
+	 * Crate of steel arrowtips
+	 */
+	public static final int CARGO_CRATE_OF_STEEL_ARROWTIPS = 31705;
+
+	/**
+	 * Crate of raw karambwan
+	 */
+	public static final int CARGO_CRATE_OF_RAW_KARAMBWAN = 31706;
+
+	/**
+	 * Crate of oak planks
+	 */
+	public static final int CARGO_CRATE_OF_OAK_PLANKS = 31707;
+
+	/**
+	 * Crate of glassmaking supplies
+	 */
+	public static final int CARGO_CRATE_OF_GLASS_MAKE_SUPPLIES = 31708;
+
+	/**
+	 * Crate of coal
+	 */
+	public static final int CARGO_CRATE_OF_COAL = 31709;
+
+	/**
+	 * Crate of silver jewellery
+	 */
+	public static final int CARGO_CRATE_OF_SILVER_JEWELLERY = 31710;
+
+	/**
+	 * Crate of buckets
+	 */
+	public static final int CARGO_CRATE_OF_BUCKETS = 31711;
+
+	/**
+	 * Crate of trawler fish
+	 */
+	public static final int CARGO_CRATE_OF_TRAWLER_FISH = 31712;
+
+	/**
+	 * Crate of gold ore
+	 */
+	public static final int CARGO_CRATE_OF_GOLD_ORE = 31713;
+
+	/**
+	 * Crate of mithril armour
+	 */
+	public static final int CARGO_CRATE_OF_MITHRIL_ARMOUR = 31714;
+
+	/**
+	 * Crate of sand
+	 */
+	public static final int CARGO_CRATE_OF_SAND = 31715;
+
+	/**
+	 * Crate of spices
+	 */
+	public static final int CARGO_CRATE_OF_SPICES = 31716;
+
+	/**
+	 * Crate of secret stuff
+	 */
+	public static final int CARGO_CRATE_OF_SECRET_STUFF = 31717;
+
+	/**
+	 * Crate of dragon daggers
+	 */
+	public static final int CARGO_CRATE_OF_DRAGON_DAGGERS = 31718;
+
+	/**
+	 * Crate of ship parts
+	 */
+	public static final int CARGO_CRATE_OF_SHIP_PARTS = 31719;
+
+	/**
+	 * Crate of dragon equipment
+	 */
+	public static final int CARGO_CRATE_OF_DRAGON_EQUIPMENT = 31720;
+
+	/**
+	 * Crate of anglers clothing
+	 */
+	public static final int CARGO_CRATE_OF_SPIRIT_ANGLERS_GARB = 31721;
+
+	/**
+	 * Crate of anglers clothing
+	 */
+	public static final int CARGO_CRATE_OF_ANGLERS_OUTFIT = 31722;
+
+	/**
+	 * Crate of pineapples
+	 */
+	public static final int CARGO_CRATE_OF_BRIMHAVEN_PINEAPPLES = 31723;
+
+	/**
+	 * Crate of high importance
+	 */
+	public static final int CARGO_CRATE_OF_HIGH_IMPORTANCE = 31724;
+
+	/**
+	 * Crate of salted meat
+	 */
+	public static final int CARGO_CRATE_OF_SALTED_MEAT = 31725;
+
+	/**
+	 * Crate of dragon's bitter
+	 */
+	public static final int CARGO_CRATE_OF_DRAGONS_BITTER = 31726;
+
+	/**
+	 * Crate of hops
+	 */
+	public static final int CARGO_CRATE_OF_HOPS = 31727;
+
+	/**
+	 * Crate of bones
+	 */
+	public static final int CARGO_CRATE_OF_BONES = 31728;
+
+	/**
+	 * Crate of steel bars
+	 */
+	public static final int CARGO_CRATE_OF_STEEL_BARS = 31729;
+
+	/**
+	 * Crate of bark
+	 */
+	public static final int CARGO_CRATE_OF_BARK = 31730;
+
+	/**
+	 * Crate of raw meat
+	 */
+	public static final int CARGO_CRATE_OF_RAW_MEAT = 31731;
+
+	/**
+	 * Crate of lumber
+	 */
+	public static final int CARGO_CRATE_OF_LUMBER = 31732;
+
+	/**
+	 * Crate of live rabbits
+	 */
+	public static final int CARGO_CRATE_OF_LIVE_RABBITS = 31733;
+
+	/**
+	 * Crate of hammers
+	 */
+	public static final int CARGO_CRATE_OF_HAMMERS = 31734;
+
+	/**
+	 * Crate of mushrooms
+	 */
+	public static final int CARGO_CRATE_OF_MUSHROOMS = 31735;
+
+	/**
+	 * Crate of fur
+	 */
+	public static final int CARGO_CRATE_OF_FUR = 31736;
+
+	/**
+	 * Crate of clothing
+	 */
+	public static final int CARGO_CRATE_OF_CLOTHING = 31737;
+
+	/**
+	 * Crate of fishing equipment
+	 */
+	public static final int CARGO_CRATE_OF_FISHING_EQUIPMENT = 31738;
+
+	/**
+	 * Crate of granite
+	 */
+	public static final int CARGO_CRATE_OF_GRANITE = 31739;
+
+	/**
+	 * Crate of importance
+	 */
+	public static final int CARGO_CRATE_OF_IMPORTANCE = 31740;
+
+	/**
+	 * Crate of cocktails
+	 */
+	public static final int CARGO_CRATE_OF_COCKTAILS = 31741;
+
+	/**
+	 * Crate of cacti
+	 */
+	public static final int CARGO_CRATE_OF_CACTUS = 31742;
+
+	/**
+	 * Crate of coral
+	 */
+	public static final int CARGO_CRATE_OF_CORAL = 31743;
+
+	/**
+	 * Crate of sandstone
+	 */
+	public static final int CARGO_CRATE_OF_SANDSTONE = 31744;
+
+	/**
+	 * Crate of crocodile meat
+	 */
+	public static final int CARGO_CRATE_OF_CROCODILE_MEAT = 31745;
+
+	/**
+	 * Crate of cactus juice
+	 */
+	public static final int CARGO_CRATE_OF_CACTUS_JUICE = 31746;
+
+	/**
+	 * Crate of yak hair
+	 */
+	public static final int CARGO_CRATE_OF_YAK_HAIR = 31747;
+
+	/**
+	 * Crate of nail beast fur
+	 */
+	public static final int CARGO_CRATE_OF_NAIL_BEAST_FUR = 31748;
+
+	/**
+	 * Crate of kyatt fur
+	 */
+	public static final int CARGO_CRATE_OF_KYATT_FUR = 31749;
+
+	/**
+	 * Crate of kebbit teeth
+	 */
+	public static final int CARGO_CRATE_OF_KEBBIT_TEETH = 31750;
+
+	/**
+	 * Crate of redwood logs
+	 */
+	public static final int CARGO_CRATE_OF_REDWOOD = 31751;
+
+	/**
+	 * Crate of aphcs rum
+	 */
+	public static final int CARGO_CRATE_OF_BRAINDEATH_RUM = 31752;
+
+	/**
+	 * Crate of kyatt teeth
+	 */
+	public static final int CARGO_CRATE_OF_KYATT_TEETH = 31753;
+
+	/**
+	 * Crate of special fish
+	 */
+	public static final int CARGO_CRATE_OF_SPECIAL_FISH = 31754;
+
+	/**
+	 * Crate of books
+	 */
+	public static final int CARGO_CRATE_OF_VARROCK_BOOKS = 31755;
+
+	/**
+	 * Crate of books
+	 */
+	public static final int CARGO_CRATE_OF_ARCEUUS_BOOKS = 31756;
+
+	/**
+	 * Crate of antelope meat
+	 */
+	public static final int CARGO_CRATE_OF_ANTELOPE_MEAT = 31757;
+
+	/**
+	 * Crate of anglerfish
+	 */
+	public static final int CARGO_CRATE_OF_ANGLERFISH = 31758;
+
+	/**
+	 * Crate of vegetables
+	 */
+	public static final int CARGO_CRATE_OF_VEGETABLES = 31759;
+
+	/**
+	 * Crate of mithril ore
+	 */
+	public static final int CARGO_CRATE_OF_MITHRIL_ORE = 31760;
+
+	/**
+	 * Crate of planks
+	 */
+	public static final int CARGO_CRATE_OF_PLANKS = 31761;
+
+	/**
+	 * Crate of flour
+	 */
+	public static final int CARGO_CRATE_OF_FLOUR = 31762;
+
+	/**
+	 * Crate of seaweed
+	 */
+	public static final int CARGO_CRATE_OF_SEAWEED = 31763;
+
+	/**
+	 * Crate of runes
+	 */
+	public static final int CARGO_CRATE_OF_VARROCK_RUNES = 31764;
+
+	/**
+	 * Crate of runes
+	 */
+	public static final int CARGO_CRATE_OF_YANILLE_RUNES = 31765;
+
+	/**
+	 * Crate of seeds
+	 */
+	public static final int CARGO_CRATE_OF_SEEDS = 31766;
+
+	/**
+	 * Crate of sharks
+	 */
+	public static final int CARGO_CRATE_OF_RELLEKKA_SHARKS = 31767;
+
+	/**
+	 * Crate of wine
+	 */
+	public static final int CARGO_CRATE_OF_WINE = 31768;
+
+	/**
+	 * Crate of chocolate
+	 */
+	public static final int CARGO_CRATE_OF_CHOCOLATE = 31769;
+
+	/**
+	 * Crate of marble
+	 */
+	public static final int CARGO_CRATE_OF_MARBLE = 31770;
+
+	/**
+	 * Crate of wheat
+	 */
+	public static final int CARGO_CRATE_OF_WHEAT = 31771;
+
+	/**
+	 * Crate of exotic furs
+	 */
+	public static final int CARGO_CRATE_OF_EXOTIC_FURS = 31772;
+
+	/**
+	 * Crate of alpaca wool
+	 */
+	public static final int CARGO_CRATE_OF_ALPACA_WOOL = 31773;
+
+	/**
+	 * Crate of pirate looty
+	 */
+	public static final int CARGO_CRATE_OF_PIRATE_LOOTY = 31774;
+
+	/**
+	 * Crate of returned bananas
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_0 = 31775;
+
+	/**
+	 * Crate of eye patches
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_1 = 31776;
+
+	/**
+	 * Crate of gold ore
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_2 = 31777;
+
+	/**
+	 * Crate of fresh pineapples
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_3 = 31778;
+
+	/**
+	 * Crate of wine jugs
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_4 = 31779;
+
+	/**
+	 * Crate of peg legs
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_5 = 31780;
+
+	/**
+	 * Crate of general store supplies
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_6 = 31781;
+
+	/**
+	 * Crate of free bananas
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_7 = 31782;
+
+	/**
+	 * Crate of artisinal beer glasses
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_8 = 31783;
+
+	/**
+	 * Crate of zembo's wine
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_9 = 31784;
+
+	/**
+	 * Crate of secret ingredients
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_10 = 31785;
+
+	/**
+	 * Crate of raw lobsters
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_11 = 31786;
+
+	/**
+	 * Crate of teak goods
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_12 = 31787;
+
+	/**
+	 * Musa point crate 13
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_13 = 31788;
+
+	/**
+	 * Crate of cocktail ingredients
+	 */
+	public static final int CARGO_CRATE_MUSA_POINT_14 = 31789;
+
+	/**
+	 * Crate of materials
+	 */
+	public static final int SAILING_INTRO_CARGO_CRATE = 31790;
+
+	/**
+	 * Famished shark fin
+	 */
+	public static final int BOUNTY_OBJECT_FAMISHED_SHARK_FIN = 31791;
+
+	/**
+	 * Crandor shark fin
+	 */
+	public static final int BOUNTY_OBJECT_CRANDOR_SHARK_FIN = 31792;
+
+	/**
+	 * Shark jaw
+	 */
+	public static final int BOUNTY_OBJECT_KARAMJAN_SHARK_JAW = 31793;
+
+	/**
+	 * Reef shark tooth
+	 */
+	public static final int BOUNTY_OBJECT_LARGE_REEF_SHARK_TEETH = 31794;
+
+	/**
+	 * Pygmy kraken tentacle
+	 */
+	public static final int BOUNTY_OBJECT_PYGMY_KRAKEN_TENTACLE = 31795;
+
+	/**
+	 * Pristine monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_PRISTINE_CATHERBY_MONODON_HORN = 31796;
+
+	/**
+	 * Broken monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_BROKEN_CATHERBY_MONODON_HORN = 31797;
+
+	/**
+	 * Pygmy kraken tentacles
+	 */
+	public static final int BOUNTY_OBJECT_PYGMY_KRAKEN_BUNCH_OF_TENTACLES = 31798;
+
+	/**
+	 * Ogre monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_OGRE_MONODON_HORN = 31799;
+
+	/**
+	 * Baby kraken head
+	 */
+	public static final int BOUNTY_OBJECT_BABY_KRAKEN_HEADS = 31800;
+
+	/**
+	 * Broken monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_BROKEN_KHARAZI_MONODON_HORN = 31801;
+
+	/**
+	 * Large reef shark fin
+	 */
+	public static final int BOUNTY_OBJECT_LARGE_REEF_SHARK_FIN = 31802;
+
+	/**
+	 * Broken monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_BROKEN_OGRE_MONODON_HORN = 31803;
+
+	/**
+	 * Baby sea kraken tentacle
+	 */
+	public static final int BOUNTY_OBJECT_BABY_SEA_KRAKEN_TENTACLE = 31804;
+
+	/**
+	 * Deep sea kraken heart
+	 */
+	public static final int BOUNTY_OBJECT_DEEP_SEA_KRAKEN_HEART = 31805;
+
+	/**
+	 * Albatross feather
+	 */
+	public static final int BOUNTY_OBJECT_ALBATROSS_FEATHER = 31806;
+
+	/**
+	 * Sea monodon heart
+	 */
+	public static final int BOUNTY_OBJECT_SEA_MONODON_HEART = 31807;
+
+	/**
+	 * Great white shark heart
+	 */
+	public static final int BOUNTY_OBJECT_GREAT_WHITE_SHARK_HEART = 31808;
+
+	/**
+	 * Karamjan tiger shark fin
+	 */
+	public static final int BOUNTY_OBJECT_KARAMJAN_TIGER_SHARK_FIN = 31809;
+
+	/**
+	 * Sea hawk feather
+	 */
+	public static final int BOUNTY_OBJECT_SEA_HAWK_FEATHER = 31810;
+
+	/**
+	 * Sea monodon horn
+	 */
+	public static final int BOUNTY_OBJECT_SEA_MONODON_HORN = 31811;
+
+	/**
+	 * Baby sea kraken blood
+	 */
+	public static final int BOUNTY_OBJECT_BABY_SEA_KRAKEN_BLOOD = 31812;
+
+	/**
+	 * Deep sea kraken tentacle
+	 */
+	public static final int BOUNTY_OBJECT_DEEP_SEA_KRAKEN_TENTACLE = 31813;
+
+	/**
+	 * Great white shark fin
+	 */
+	public static final int BOUNTY_OBJECT_GREAT_WHITE_SHARK_FIN = 31814;
+}

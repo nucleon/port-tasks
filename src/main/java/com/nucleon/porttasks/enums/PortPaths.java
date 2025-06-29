26425eb48d6f95fbed2bccc82cb7f4fe32c866a9
coopermor <coopermorris96@yahoo.com>
2025-06-28 18:27:45 -0700

    formatting

--- src/main/java/com/nucleon/porttasks/enums/PortPaths.java
+++ src/main/java/com/nucleon/porttasks/enums/PortPaths.java
@@ -132,24 +132,19 @@ public enum PortPaths
 	),
 
 	ARDOUGNE_RUINS_OF_UNKAH(
-			PortLocation.ARDOUGNE,
-			PortLocation.RUINS_OF_UNKAH,
-
-			new RelativeMove(90, 0),
-			new RelativeMove(34, -34),
-			new RelativeMove(17, 0),
-			new RelativeMove(12, -12),
-
-			new RelativeMove(85, 0),
-			new RelativeMove(55, -55),
-
-			new RelativeMove(0, -42),
-			new RelativeMove(121, -121),
-			new RelativeMove(0, -20),
-			new RelativeMove(43, -43),
-			new RelativeMove(0, -108)
-
-
+		PortLocation.ARDOUGNE,
+		PortLocation.RUINS_OF_UNKAH,
+		new RelativeMove(90, 0),
+		new RelativeMove(34, -34),
+		new RelativeMove(17, 0),
+		new RelativeMove(12, -12),
+		new RelativeMove(85, 0),
+		new RelativeMove(55, -55),
+		new RelativeMove(0, -42),
+		new RelativeMove(121, -121),
+		new RelativeMove(0, -20),
+		new RelativeMove(43, -43),
+		new RelativeMove(0, -108)
 	),
 
 	ENTRANA_MUSA_POINT(
@@ -230,7 +225,6 @@ public enum PortPaths
 		new RelativeMove(-40, -40),
 		new RelativeMove(0, -15),
 		new RelativeMove(5, -5)
-
 	),
 
 	PANDEMONIUM_RUINS_OF_UNKAH(

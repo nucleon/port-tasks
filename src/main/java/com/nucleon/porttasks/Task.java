0dc3ca7633d8d2f060b092322161513aefdee4a1
coopermor <coopermorris96@yahoo.com>
2025-11-20 17:56:31 -0800

    Formatting

--- src/main/java/com/nucleon/porttasks/Task.java
+++ src/main/java/com/nucleon/porttasks/Task.java
@@ -2,7 +2,7 @@ package com.nucleon.porttasks;
 
 public interface Task
 {
-int getSlot();
-boolean isTracking();
-void setTracking(boolean tracking);
+	int getSlot();
+	boolean isTracking();
+	void setTracking(boolean tracking);
 }

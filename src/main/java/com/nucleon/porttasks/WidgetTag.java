0ddf7195078b1a6154856d46107125a4a376a44b
coopermor <coopermorris96@yahoo.com>
2025-11-25 13:43:23 -0800

    Add ability to tag tasks

---
+++ src/main/java/com/nucleon/porttasks/WidgetTag.java
@@ -0,0 +1,15 @@
+package com.nucleon.porttasks;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.Color;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WidgetTag {
+	private int dbrow;
+	private Color color;
+}
